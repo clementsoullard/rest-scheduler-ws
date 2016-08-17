f2c0f729e958854f7a03031a70fb3d8e955bbdc4
cleme <cleme@DESKTOP-1Q11J5U.home>
2016-08-17 23:00:54 +0200

    Debut du dialogue pin

--- src/main/webapp/view1/view1.js
+++ src/main/webapp/view1/view1.js
@@ -9,7 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl',  ['$scope','$http', function($scope,$http) {
+.controller('View1Ctrl',  ['$scope','$http', '$mdDialog', '$mdMedia', function($scope,$http, $mdDialog, $mdMedia) {
 
    $http.get('/tvscheduler/tvstatus').
         success(function(data) {
@@ -34,5 +34,85 @@ angular.module('myApp.view1', ['ngRoute'])
 			$scope.error = true;
 		})
 		};
-			
+/**
+ * Confirmation dialog
+ */
+		  $scope.showPrompt = function(ev,point,rationale) {
+			    // Appending dialog to document.body to cover sidenav in docs app
+			  var rnd0=Math.floor((Math.random() * 10) );
+			  var rnd1=Math.floor((Math.random() * 10) );
+			  var requiredPin=1000+rnd1*100+rnd0*10+1;
+			  var confirm = $mdDialog.prompt()
+			      .title('Code de confirmation')
+			      .textContent('Entrez le code de confirmation '+rnd1 +''+rnd0)
+			      .placeholder('Code confirmation')
+			      .ariaLabel('Code confirmation')
+				      .targetEvent(ev)
+			      .ok('Ok')
+			      .cancel('Annuler');
+			    $mdDialog.show(confirm).then(function(result) {
+			    console.log(result);
+			    if(result==requiredPin){
+			        $scope.punition(point,rationale);
+			    }else{
+			    	console.log('refusé'+requiredPin);
+			    	$scope.punitionMessage = 'Le code pin entré nest pas correct';
+					$scope.error = true;
+
+			    }
+						    
+			    }, function() {
+			      $scope.status = 'You didn\'t name your dog.';
+			    });
+			  };
+			  
+			  $scope.showAdvanced = function(ev,point,rationale) {
+				    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+				    $mdDialog.show({
+				       controller: DialogController,
+				       templateUrl: 'view1/pin.tmpl.html',
+				      parent: angular.element(document.body),
+				      targetEvent: ev,
+				      clickOutsideToClose:true,
+				      fullscreen: useFullScreen
+				    })
+				    .then(function(result,requiredPin) {
+					    console.log(result);
+					
+								    
+					    }, function() {
+					      $scope.status = 'You didn\'t name your dog.';
+					    });
+			  };
+		
 }]);
+
+
+
+function DialogController($scope, $mdDialog) {
+    // Appending dialog to document.body to cover sidenav in docs app
+	  var rnd0=Math.floor((Math.random() * 10) );
+	  var rnd1=Math.floor((Math.random() * 10) );
+	  var requiredPin=rnd1*10+rnd0*1;
+	  $scope.requiredPin=requiredPin;
+
+	
+	  $scope.hide = function() {
+	    $mdDialog.hide();
+	  };
+	  $scope.cancel = function() {
+	    $mdDialog.cancel();
+	  };
+	  $scope.answer = function(answer,requiredPin) {
+		  var completePin=$scope.requiredPin*10 + 1001;
+		  if(answer==completePin){
+		        $scope.punition(point,rationale);
+		    }else{
+		    	console.log('refusé'+ completePin + ' '+answer);
+		    	$scope.punitionMessage = 'Le code pin entré nest pas correct';
+				$scope.error = true;
+
+		    }
+	    $mdDialog.hide(answer);
+	  };
+	}
